a8c02a29e20d1a8c5025d3dc7dfb7aeb46702366
Rodrigo Pinto <zimmer672@gmail.com>
2021-05-22 17:24:51 -0400

    added calendar func

---
+++ front/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
+
+
+urlpatterns = [
+
+    url(r'^$', index, name='index'),
+    url('about', about, name='about'),
+    url('portfolio', portfolio, name='portfolio'),
+    url('contact', contact, name='contact'),
+    url('calendar', calendar, name='calendar'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
